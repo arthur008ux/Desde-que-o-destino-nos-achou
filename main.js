52e3fb83ea927068a3dad44c1b9ab1b10d146dd7
Arthur008x <arthursla878@gmail.com>
2025-05-29 23:50:38 -0300

    v1

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function iniciarContagem(dataInicial){
+function atualizarContagem(){
+
+    
+    let data = new Date()
+
+    let diferencaMs = data - dataInicial
+    const segundosTotais = Math.floor(diferencaMs / 1000)
+    let dias = Math.floor(segundosTotais / 86400).toString().padStart(2,'0')
+    let horas = Math.floor((segundosTotais % 86400) / 3600 ).toString().padStart(2,'0')
+    let minutos =  Math.floor((segundosTotais % 3600) / 60).toString().padStart(2,'0')
+    let segundos = (segundosTotais % 60).toString().padStart(2,'0')
+    
+    document.getElementById("relogioId").textContent = (`${dias} Dias: ${horas} Horas:  ${minutos} Minutos:  ${segundos} Segundos `);
+
+}
+
+setInterval(atualizarContagem, 1000)
+
+atualizarContagem()
+}
+
+const dataInicial = new Date(2025, 4, 3, 19, 3, 0)
+iniciarContagem(dataInicial)
