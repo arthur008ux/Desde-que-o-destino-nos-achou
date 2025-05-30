199aef5f501a27f204459ed5e4e921131522db17
Arthur008x <arthursla878@gmail.com>
2025-05-30 10:27:02 -0300

    v2

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ function atualizarContagem(){
 
     let diferencaMs = data - dataInicial
     const segundosTotais = Math.floor(diferencaMs / 1000)
-    let dias = Math.floor(segundosTotais / 86400).toString().padStart(2,'0')
+    let meses = Math.floor(segundosTotais / 2592000).toString().padStart(2,'0')
+    let dias = Math.floor((segundosTotais % 2592000) / 86400).toString().padStart(2,'0')
     let horas = Math.floor((segundosTotais % 86400) / 3600 ).toString().padStart(2,'0')
     let minutos =  Math.floor((segundosTotais % 3600) / 60).toString().padStart(2,'0')
     let segundos = (segundosTotais % 60).toString().padStart(2,'0')
     
-    document.getElementById("relogioId").textContent = (`${dias} Dias: ${horas} Horas:  ${minutos} Minutos:  ${segundos} Segundos `);
+    document.getElementById("relogioId").textContent = (`${meses} Meses: ${dias} Dias: ${horas} Horas:  ${minutos} Minutos:  ${segundos} Segundos `);
 
 }
 
